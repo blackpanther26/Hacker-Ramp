f86ecb7bcc206882572a6e656eab468c0b76222e
blackpanther26 <priyanshuchahal1@gmail.com>
2024-07-09 23:54:30 +0530

    adds auth page and home page

---
+++ src/components/FeedPosts/PostHeader.jsx
@@ -0,0 +1,35 @@
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+
+const PostHeader = () => {
+  return (
+    <Flex
+      justifyContent={"space-between"}
+      alignItems={"center"}
+      w={"full"}
+      my={2}
+    >
+      <Flex alignItems={"center"} gap={2}>
+        <Avatar src="/img1.png" alt="user profile pic" size={"sm"} />
+        <Flex fontSize={15} fontWeight={"bold"} gap={2}>
+          Pixel
+        </Flex>
+        <Box color={"gray.500"}>• 2h</Box>
+      </Flex>
+      <Box cursor={"pointer"}>
+        <Text
+          fontSize={12}
+          color={"blue.500"}
+          fontWeight={"bold"}
+          _hover={{
+            color: "white",
+          }}
+          transition={"0.2s ease-in-out"}
+        >
+          Unfollow
+        </Text>
+      </Box>
+    </Flex>
+  );
+};
+
+export default PostHeader;
